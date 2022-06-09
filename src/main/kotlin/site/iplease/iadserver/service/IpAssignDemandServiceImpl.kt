df57ee5f460e470c32795a7d850488740a7a86f6
raul <develop.raul@gmail.com>
2022-06-10 00:30:53 +0900

    [ADD] entity추가시 id설정로직 및 주석추가

--- src/main/kotlin/site/iplease/iadserver/service/IpAssignDemandServiceImpl.kt
+++ src/main/kotlin/site/iplease/iadserver/service/IpAssignDemandServiceImpl.kt
@@ -11,8 +11,10 @@ class IpAssignDemandServiceImpl(
     private val demandRepository: DemandRepository,
     private val demandDataConverter: DemandDataConverter
 ): IpAssignDemandService {
+    //신청을 추가한다.
     override fun addDemand(demand: DemandDto): Mono<DemandDto> =
-        demandDataConverter.toEntity(demand)
-            .flatMap { entity -> demandRepository.save(entity) }
-            .flatMap { entity -> demandDataConverter.toDto(entity) }
+        demandDataConverter.toEntity(demand) //converter를 통해, 파라미터로 받은 dto를 entity로 치환한다.
+            .map { entity -> entity.copy(id = 0) } //새로운 신청을 추가해야하므로, id를 0으로 설정한다.
+            .flatMap { entity -> demandRepository.save(entity) } //구성한 entity를 repository에 저장한다.
+            .flatMap { entity -> demandDataConverter.toDto(entity) } //저장한 entity를 dto로 치환한다.
 }
