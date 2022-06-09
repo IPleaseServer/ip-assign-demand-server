5d2a564594f0711c8ac8d1f87c12f2d1b6b8e21a
raul <develop.raul@gmail.com>
2022-06-09 16:16:56 +0900

    [ADD] DemandDataConverter 스팩변경에 따른 구현체 추가구현사항 반영

--- src/main/kotlin/site/iplease/iadserver/util/DemandDataConverterImpl.kt
+++ src/main/kotlin/site/iplease/iadserver/util/DemandDataConverterImpl.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import site.iplease.iadserver.data.dto.DemandDto
+import site.iplease.iadserver.data.entity.Demand
 import site.iplease.iadserver.data.request.AssignIpDemandRequest
 
 @Component
@@ -16,4 +17,24 @@ class DemandDataConverterImpl: DemandDataConverter {
             usage = request.usage,
             expireAt = request.expireAt
         ) }
+
+    override fun toDto(entity: Demand): Mono<DemandDto> =
+        entity.toMono().map { DemandDto(
+                id = it.id,
+                issuerId = it.issuerId,
+                title = it.title,
+                description = it.description,
+                usage = it.usage,
+                expireAt = it.expireAt
+            ) }
+
+    override fun toEntity(dto: DemandDto): Mono<Demand> =
+        dto.toMono().map { Demand(
+                id = it.id,
+                issuerId = it.issuerId,
+                title = it.title,
+                description = it.description,
+                usage = it.usage,
+                expireAt = it.expireAt
+            ) }
 }
