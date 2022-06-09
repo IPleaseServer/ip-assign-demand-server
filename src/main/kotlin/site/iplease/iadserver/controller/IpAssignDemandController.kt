43fbb2e4c6029f22e40d0795c6c970daa6e9ea55
raul <develop.raul@gmail.com>
2022-06-10 00:32:15 +0900

    [ADD] 비즈니스로직 처리 후 message 발행로직 추가 및 이에 필요한 타입/인터페이스 작성
    - MessageType 작성
    - MessagePublishService 작성

--- src/main/kotlin/site/iplease/iadserver/controller/IpAssignDemandController.kt
+++ src/main/kotlin/site/iplease/iadserver/controller/IpAssignDemandController.kt
@@ -6,15 +6,18 @@ import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 import site.iplease.iadserver.data.request.AssignIpDemandRequest
 import site.iplease.iadserver.data.response.AssignIpDemandResponse
+import site.iplease.iadserver.data.type.MessageType
 import site.iplease.iadserver.service.IpAssignDemandService
-import site.iplease.iadserver.util.DemandDataValidator
+import site.iplease.iadserver.service.MessagePublishService
 import site.iplease.iadserver.util.DemandDataConverter
+import site.iplease.iadserver.util.DemandDataValidator
 
 @Validated
 @RestController
 @RequestMapping("/api/v1/demand/assign")
 class IpAssignDemandController(
     private val ipAssignDemandService: IpAssignDemandService,
+    private val messagePublishService: MessagePublishService,
     private val demandDataConverter: DemandDataConverter,
     private val demandDataValidator: DemandDataValidator
 ) {
@@ -25,6 +28,7 @@ class IpAssignDemandController(
             demandDataConverter.toDto(accountId, request)
                 .flatMap { demand -> demandDataValidator.validate(demand) }
                 .flatMap { demand -> ipAssignDemandService.addDemand(demand) }
+                .flatMap { demand -> messagePublishService.publish(MessageType.DEMAND_ASSIGN_IP, demand).map { demand } }
                 .map { demand -> AssignIpDemandResponse(demandId = demand.id) }
                 .map { response -> ResponseEntity.ok(response) }
 }
