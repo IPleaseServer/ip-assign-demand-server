3713d4e14ee38e536f7333f83ab182c3200b14a0
raul <develop.raul@gmail.com>
2022-06-10 14:54:08 +0900

    [FIX] 잘못된 basePackageClasses수정

--- src/main/kotlin/site/iplease/iadserver/advice/IpAssignDemandControllerAdvice.kt
+++ src/main/kotlin/site/iplease/iadserver/advice/IpAssignDemandControllerAdvice.kt
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
+import site.iplease.iadserver.controller.IpAssignDemandController
 import site.iplease.iadserver.exception.WrongExpireDateException
 import site.iplease.iadserver.exception.WrongTitleException
 
-@RestControllerAdvice(basePackageClasses = [IpAssignDemandControllerAdvice::class])
+@RestControllerAdvice(basePackageClasses = [IpAssignDemandController::class])
 class IpAssignDemandControllerAdvice {
     @ExceptionHandler(WrongExpireDateException::class)
     fun handle(exception: WrongExpireDateException): Mono<ResponseEntity<String>> =
