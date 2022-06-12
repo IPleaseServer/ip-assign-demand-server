aae9a71d1d64751d760752b3d6aa5f5ab9ff6625
raul <develop.raul@gmail.com>
2022-06-12 17:30:37 +0900

    [FIX] 패키지 구조 변경

--- src/main/kotlin/site/iplease/iadserver/domain/demand/util/DemandErrorOnStatusConverter.kt
+++ src/main/kotlin/site/iplease/iadserver/domain/demand/util/DemandErrorOnStatusConverter.kt
@@ -2,7 +2,7 @@ package site.iplease.iadserver.domain.demand.util
 
 import reactor.core.publisher.Mono
 import site.iplease.iadserver.domain.demand.data.dto.IpAssignDemandErrorOnStatusDto
-import site.iplease.iadserver.global.demand.message.IpAssignDemandErrorOnStatusMessage
+import site.iplease.iadserver.global.demand.data.message.IpAssignDemandErrorOnStatusMessage
 
 interface DemandErrorOnStatusConverter {
     fun convert(message: IpAssignDemandErrorOnStatusMessage): Mono<IpAssignDemandErrorOnStatusDto>
