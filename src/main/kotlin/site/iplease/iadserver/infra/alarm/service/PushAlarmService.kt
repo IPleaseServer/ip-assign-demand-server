904c236392f873b3932fc0d9d42e062260952b51
raul <develop.raul@gmail.com>
2022-06-14 11:04:50 +0900

    [ADD] PushAlarmService 구현 및 이에 필요한 컴포넌트 작성
    - SendAlarmMessage 작성
    - MessageType에 SEND_ALARM Constant추가
    - AlarmType 작성

--- src/main/kotlin/site/iplease/iadserver/infra/alarm/service/PushAlarmService.kt
+++ src/main/kotlin/site/iplease/iadserver/infra/alarm/service/PushAlarmService.kt
@@ -3,5 +3,5 @@ package site.iplease.iadserver.infra.alarm.service
 import reactor.core.publisher.Mono
 
 interface PushAlarmService {
-    fun publish(title: String, description: String): Mono<Unit>
+    fun publish(receiverId: Long, title: String, description: String): Mono<Unit>
 }
