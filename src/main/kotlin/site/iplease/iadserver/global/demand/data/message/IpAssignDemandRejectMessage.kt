f8ce82e4c3c897164cea6e8bfffc517d656af15f
raul <develop.raul@gmail.com>
2022-06-18 09:56:00 +0900

    [ADD] IpAssignDemandRejectSubscriber인터페이스 작성 및 이에 필요한 객체 작성
    - IpAssignDemandRejectMessage 작성
    - DemandStatusType 작성

---
+++ src/main/kotlin/site/iplease/iadserver/global/demand/data/message/IpAssignDemandRejectMessage.kt
@@ -0,0 +1,9 @@
+package site.iplease.iadserver.global.demand.data.message
+
+import site.iplease.iadserver.global.demand.data.type.DemandStatusType
+
+data class IpAssignDemandRejectMessage(
+    val demandId: Long,
+    val originStatus: DemandStatusType,
+    val reason: String
+)
