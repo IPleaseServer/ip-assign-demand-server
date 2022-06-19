7d3c56346bcdd1dcf37a86311733b13916dc25ed
raul <develop.raul@gmail.com>
2022-06-19 11:00:07 +0900

    [FIX] RejectedDemandProperties prefix수정

--- src/main/kotlin/site/iplease/iadserver/domain/demand/config/RejectedDemandProperties.kt
+++ src/main/kotlin/site/iplease/iadserver/domain/demand/config/RejectedDemandProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
-@ConfigurationProperties(prefix = "rejected-demand")
+@ConfigurationProperties(prefix = "iplease.rejected-demand")
 class RejectedDemandProperties(
     val redisKeyPrefix: String
 ) {
