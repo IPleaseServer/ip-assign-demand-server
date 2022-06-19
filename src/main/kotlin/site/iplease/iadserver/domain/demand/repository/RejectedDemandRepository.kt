d2e2a8094a0fb9701b9a88c65815dbf28d96813f
raul <develop.raul@gmail.com>
2022-06-19 14:41:26 +0900

    [FIX] RemoveRejectedDemandStrategyImpl 로직수정 및 이에 필요한 인터페이스 스팩추가
    - RejectedDemandRepository 에 delete 스팩추가

--- src/main/kotlin/site/iplease/iadserver/domain/demand/repository/RejectedDemandRepository.kt
+++ src/main/kotlin/site/iplease/iadserver/domain/demand/repository/RejectedDemandRepository.kt
@@ -8,4 +8,5 @@ interface RejectedDemandRepository {
     fun insert(rejectedDemand: RejectedDemand): Mono<Void>
     fun exist(id: Long): Mono<Boolean>
     fun selectAll(): Flux<RejectedDemand>
+    fun delete(demandId: Long): Mono<Long>
 }
--- src/main/kotlin/site/iplease/iadserver/domain/demand/strategy/RemoveRejectedDemandStrategyImpl.kt
+++ src/main/kotlin/site/iplease/iadserver/domain/demand/strategy/RemoveRejectedDemandStrategyImpl.kt
@@ -3,6 +3,7 @@ package site.iplease.iadserver.domain.demand.strategy
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import reactor.core.scheduler.Schedulers
+import reactor.kotlin.core.publisher.toFlux
 import reactor.kotlin.core.publisher.toMono
 import site.iplease.iadserver.domain.demand.repository.DemandRepository
 import site.iplease.iadserver.domain.demand.repository.RejectedDemandRepository
@@ -14,11 +15,11 @@ class RemoveRejectedDemandStrategyImpl(
 ): RemoveRejectedDemandStrategy {
     private val logger = LoggerFactory.getLogger(this::class.java)
     override fun removeRejectedDemand() {
-        rejectedDemandRepository.selectAll()
-            .parallel()
-            .runOn(Schedulers.parallel())
+        Unit.toMono()
+            .map { logger.info("거절된 예약에 대한 일괄삭제작업을 진행합니다!") }.toFlux()
+            .flatMap { rejectedDemandRepository.selectAll() }
             .flatMap { rejectedDemand -> demandRepository.deleteByIdentifier(rejectedDemand.demandId).then(rejectedDemand.toMono()) }
-            .doOnRequest { logger.info("거절된 예약에 대한 일괄삭제작업을 진행합니다!") }
+            .flatMap { rejectedDemand ->  rejectedDemandRepository.delete(rejectedDemand.demandId).then(rejectedDemand.toMono()) }
             .subscribe (
                 { rejectedDemand -> logger.trace("Id가 ${rejectedDemand.demandId}인 예약을 제거하였습니다!") },
                 { throwable ->
