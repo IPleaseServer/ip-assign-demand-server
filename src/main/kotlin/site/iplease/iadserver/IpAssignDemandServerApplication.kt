62214e813940a6e1a31c47e69ba7523ed2e9549d
raul <develop.raul@gmail.com>
2022-06-19 14:40:52 +0900

    [ADD] Scheduler활성화

--- src/main/kotlin/site/iplease/iadserver/IpAssignDemandServerApplication.kt
+++ src/main/kotlin/site/iplease/iadserver/IpAssignDemandServerApplication.kt
@@ -3,9 +3,11 @@ package site.iplease.iadserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 import site.iplease.iadserver.domain.demand.config.RejectedDemandProperties
 
 @SpringBootApplication
+@EnableScheduling
 @EnableConfigurationProperties(RejectedDemandProperties::class)
 class IpAssignDemandServerApplication
 
