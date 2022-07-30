0962d486ab8f0a01e72599fcc7965e52e08cf1d9
raul <develop.raul@gmail.com>
2022-07-30 20:50:36 +0900

    [ADD] 정책 검사 관련 테스트 작성
    - IP할당예약취소정책 검사 추가 테스트 작성
    - IP할당예약취소정책 검사 거절 테스트 작성
    - IP할당예약취소정책 검사 수락 테스트 작성

--- src/test/kotlin/site/iplease/iadserver/domain/common/util/DemandPolicyValidatorImplTest.kt
+++ src/test/kotlin/site/iplease/iadserver/domain/common/util/DemandPolicyValidatorImplTest.kt
@@ -1,5 +1,6 @@
 package site.iplease.iadserver.domain.common.util
 
+import TestUtil
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
@@ -15,6 +16,7 @@ import site.iplease.iadserver.global.common.data.type.AssignIpUsageType
 import site.iplease.iadserver.global.common.data.type.DemandPolicyType
 import site.iplease.iadserver.global.common.repository.DemandRepository
 import site.iplease.iadserver.global.common.util.DateUtil
+import java.time.LocalDate
 import kotlin.random.Random
 
 class DemandPolicyValidatorImplTest {
@@ -34,7 +36,7 @@ class DemandPolicyValidatorImplTest {
         val title = listOf("제목일지도", "제목일거야", "제목").random()
         val description = listOf("설명일지도", "설명일거야", "설명").random()
         val usage = AssignIpUsageType.values().random()
-        val expireAt = TestUtil.randomPastDate()
+        val expireAt = TestUtil.randomDate()
         randomDto = DemandDto(demandId, issuerId, title, description, usage, expireAt)
     }
 
@@ -48,6 +50,10 @@ class DemandPolicyValidatorImplTest {
         //취소정책 검사에 성공하였다면, 예약정보를 반환한다.
         //취소정책 검사에 성공하려면 아래의 작업이 성공해야한다.
         //- 예약취소정책 검사 트랜잭션 수행
+
+        //예약취소정책 검사 트랜잭션이 성공하려면, 아래의 조건을 만족해야한다.
+        //- 예약이 존재할 것
+        //- issuer가 예약의 issuer(소유자)일 것
 
         //given
         val dto = randomDto.copy()
@@ -77,47 +83,78 @@ class DemandPolicyValidatorImplTest {
         //추가정책 검사에 성공하려면 아래의 작업이 성공해야한다.
         //- 예약추가정책 검사 트랜잭션 수행
 
+        //예약추가정책 검사 트랜잭션이 성공하려면, 아래의 조건을 만족해야한다.
+        //- expireAt이 오늘 이후일 것
+        //- title이 25자 이하일 것
+
         //given
+        val now = mock<LocalDate>()
+        val expireAt = mock<LocalDate>()
+        val dto = randomDto.copy(expireAt = expireAt)
 
         //when
+        whenever(dateUtil.dateNow()).thenReturn(now)
+        whenever(expireAt.isAfter(now)).thenReturn(true)
+
+        val result = target.validate(dto, DemandPolicyType.DEMAND_CREATE).block()!!
 
         //then
-
+        assertEquals(result, dto.copy(id=0))
+        verify(dateUtil, times(1)).dateNow()
+        verify(expireAt, times(1)).isAfter(now)
     }
 
     //validateReject 로직
     //예약 존재여부를 검사한다.
     //예약정보를 반환한다.
 
+    //TODO 예약거절정책에 수락된 예약에 존재하지 않을 것 조건추가하기
     @Test @DisplayName("IP할당예약거절정책 검사 성공 테스트")
     fun testValidateRejectSuccess() {
         //거절정책 검사에 성공하였다면, 예약정보를 반환한다.
         //거절정책 검사에 성공하려면 아래의 작업이 성공해야한다.
         //- 예약거절정책 검사 트랜잭션 수행
 
+        //예약거절정책 검사 트랜잭션이 성공하려면, 아래의 조건을 만족해야한다.
+        //- 예약이 존재할 것
+
         //given
+        val dto = randomDto.copy()
 
         //when
+        whenever(demandRepository.existsByIdentifier(dto.id)).thenReturn(true.toMono())
+
+        val result = target.validate(dto, DemandPolicyType.DEMAND_REJECT).block()!!
 
         //then
-
+        assertEquals(result, dto)
+        verify(demandRepository, times(1)).existsByIdentifier(dto.id)
     }
 
     //validateAccept 로직
     //예약 존재여부를 검사한다.
     //예약정보를 반환한다.
 
+    //TODO 예약수락정책에 거절된 예약에 존재하지 않을 것 조건추가하기
     @Test @DisplayName("IP할당예약수락정책 검사 성공 테스트")
     fun testValidateAcceptSuccess() {
         //수락정책 검사에 성공하였다면, 예약정보를 반환한다.
         //수락정책 검사에 성공하려면 아래의 작업이 성공해야한다.
         //- 예약수락정책 검사 트랜잭션 수행
 
+        //예약수락정책 검사 트랜잭션이 성공하려면, 아래의 조건을 만족해야한다.
+        //- 예약이 존재할 것
+
         //given
+        val dto = randomDto.copy()
 
         //when
+        whenever(demandRepository.existsByIdentifier(dto.id)).thenReturn(true.toMono())
+
+        val result = target.validate(dto, DemandPolicyType.DEMAND_ACCEPT).block()!!
 
         //then
-
+        assertEquals(result, dto)
+        verify(demandRepository, times(1)).existsByIdentifier(dto.id)
     }
 }
