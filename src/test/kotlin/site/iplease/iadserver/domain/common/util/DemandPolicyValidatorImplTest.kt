16a68440deac7e4501dba5769a73e8d957fdf020
raul <develop.raul@gmail.com>
2022-07-30 20:32:03 +0900

    [ADD] IP할당예약취소정책 검사 성공 테스트 작성

---
+++ src/test/kotlin/site/iplease/iadserver/domain/common/util/DemandPolicyValidatorImplTest.kt
@@ -0,0 +1,123 @@
+package site.iplease.iadserver.domain.common.util
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import reactor.kotlin.core.publisher.toMono
+import site.iplease.iadserver.DemandTestUtil
+import site.iplease.iadserver.global.common.data.dto.DemandDto
+import site.iplease.iadserver.global.common.data.type.AssignIpUsageType
+import site.iplease.iadserver.global.common.data.type.DemandPolicyType
+import site.iplease.iadserver.global.common.repository.DemandRepository
+import site.iplease.iadserver.global.common.util.DateUtil
+import kotlin.random.Random
+
+class DemandPolicyValidatorImplTest {
+    private lateinit var demandRepository: DemandRepository
+    private lateinit var dateUtil: DateUtil
+    private lateinit var target: DemandPolicyValidatorImpl
+    private lateinit var randomDto: DemandDto
+
+    @BeforeEach
+    fun beforeEach() {
+        demandRepository = mock()
+        dateUtil = mock()
+        target = DemandPolicyValidatorImpl(demandRepository, dateUtil)
+
+        val demandId = Random.nextLong()
+        val issuerId = Random.nextLong()
+        val title = listOf("제목일지도", "제목일거야", "제목").random()
+        val description = listOf("설명일지도", "설명일거야", "설명").random()
+        val usage = AssignIpUsageType.values().random()
+        val expireAt = TestUtil.randomPastDate()
+        randomDto = DemandDto(demandId, issuerId, title, description, usage, expireAt)
+    }
+
+    //validateCancel 로직
+    //예약 존재여부를 검사한다.
+    //예약 소유여부를 검사한다.
+    //예약정보를 반환한다.
+
+    @Test @DisplayName("IP할당예약취소정책 검사 성공 테스트")
+    fun testValidateCancelSuccess() {
+        //취소정책 검사에 성공하였다면, 예약정보를 반환한다.
+        //취소정책 검사에 성공하려면 아래의 작업이 성공해야한다.
+        //- 예약취소정책 검사 트랜잭션 수행
+
+        //given
+        val dto = randomDto.copy()
+        val entity = DemandTestUtil.toEntity(1, dto)
+
+        //when
+        whenever(demandRepository.existsByIdentifier(dto.id)).thenReturn(true.toMono())
+        whenever(demandRepository.findByIdentifier(dto.id)).thenReturn(entity.toMono())
+
+        val result = target.validate(dto, DemandPolicyType.DEMAND_CANCEL).block()!!
+
+        //then
+        assertEquals(result, dto)
+        verify(demandRepository, times(1)).existsByIdentifier(dto.id)
+        verify(demandRepository, times(1)).findByIdentifier(dto.id)
+    }
+
+    //validateCreate 로직
+    //예약 만료기한을 검사한다.
+    //예약 제목을 검사한다.
+    //예약정보를 변조한다. (id = 0)
+    //예약정보를 반환한다.
+
+    @Test @DisplayName("IP할당예약추가정책 검사 성공 테스트")
+    fun testValidateCreateSuccess() {
+        //추가정책 검사에 성공하였다면, 예약정보를 반환한다.
+        //추가정책 검사에 성공하려면 아래의 작업이 성공해야한다.
+        //- 예약추가정책 검사 트랜잭션 수행
+
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    //validateReject 로직
+    //예약 존재여부를 검사한다.
+    //예약정보를 반환한다.
+
+    @Test @DisplayName("IP할당예약거절정책 검사 성공 테스트")
+    fun testValidateRejectSuccess() {
+        //거절정책 검사에 성공하였다면, 예약정보를 반환한다.
+        //거절정책 검사에 성공하려면 아래의 작업이 성공해야한다.
+        //- 예약거절정책 검사 트랜잭션 수행
+
+        //given
+
+        //when
+
+        //then
+
+    }
+
+    //validateAccept 로직
+    //예약 존재여부를 검사한다.
+    //예약정보를 반환한다.
+
+    @Test @DisplayName("IP할당예약수락정책 검사 성공 테스트")
+    fun testValidateAcceptSuccess() {
+        //수락정책 검사에 성공하였다면, 예약정보를 반환한다.
+        //수락정책 검사에 성공하려면 아래의 작업이 성공해야한다.
+        //- 예약수락정책 검사 트랜잭션 수행
+
+        //given
+
+        //when
+
+        //then
+
+    }
+}
