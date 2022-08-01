f3d34de6719a9d9b8d78bdc31f846fa4774a099e
raul <develop.raul@gmail.com>
2022-08-01 18:25:09 +0900

    [FIX] 할당IP 생성로직 고도화

--- src/test/kotlin/TestDummyDataUtil.kt
+++ src/test/kotlin/TestDummyDataUtil.kt
@@ -4,11 +4,11 @@ import java.time.LocalDate
 import kotlin.random.Random
 
 object TestDummyDataUtil {
-    fun reason() = listOf("날이 좋아서", "날이 좋지 않아서", "날이 좋아서", "그냥").random()
+    fun reason() = listOf("날이 좋아서", "날이 좋지 않아서", "날이 적당해서", "그냥").random()
     fun title() = listOf("제목", "제목일거야", "제목일지도", "제목인걸까", "제목인거야").random()
     fun description() = listOf("내용", "내용일거야", "내용일지도", "내용인걸까", "내용인거야").random()
     fun errorMessage() = listOf("에러메시지", "에러메시지일거야", "에러메시지일지도", "에러메시지인걸까", "에러메시지인거야").random()
-    fun assignIp() = listOf("127.0.0.1", "192.168.12.4", "10.28.87.61", "142.250.206.196").random()
+    fun assignIp() = String.format("%s.%s.%s.%s", Random.nextInt(0, 255), Random.nextInt(0, 255), Random.nextInt(0, 255), Random.nextInt(0, 255))
     fun id() = Random.nextLong(1, Long.MAX_VALUE)
     fun demandStatus() = DemandStatusType.values().random()
     fun usage() = AssignIpUsageType.values().random()
