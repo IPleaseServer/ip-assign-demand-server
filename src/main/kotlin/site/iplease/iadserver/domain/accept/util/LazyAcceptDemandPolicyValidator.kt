f809a6d11c65a126af7b3efe98e027a75075eb47
raul <develop.raul@gmail.com>
2022-08-01 19:02:28 +0900

    [FIX] 주석 및 로깅 메세지 수정

--- src/main/kotlin/site/iplease/iadserver/domain/accept/util/LazyAcceptDemandPolicyValidator.kt
+++ src/main/kotlin/site/iplease/iadserver/domain/accept/util/LazyAcceptDemandPolicyValidator.kt
@@ -35,8 +35,8 @@ class LazyAcceptDemandPolicyValidator(
                 if(isExists == beExists) Unit.toMono()
                 else {// StatusManage서비스와 일관성이 깨져있을떄 발생함
                     logger.warn("IpAssignDemandStatusManageService와 IpAssignDemandService간의 데이터 정합성이 파손되었습니다.")
-                    logger.warn("이미 ACCEPT된 예약에 대한 ACCEPT Operation이 실행되었습니다.")
-                    return@flatMap Mono.error(AlreadyRejectedDemandException("수락하신 예약은 재수락하실 수 없습니다! - ${demand.id}"))
+                    logger.warn("이미 ACCEPT/REJECT된 예약에 대한 ACCEPT Operation이 실행되었습니다.")
+                    return@flatMap Mono.error(AlreadyRejectedDemandException("수락하거나 거절하신 예약은 재수락하실 수 없습니다! - ${demand.id}"))
                 }
             }
 }
