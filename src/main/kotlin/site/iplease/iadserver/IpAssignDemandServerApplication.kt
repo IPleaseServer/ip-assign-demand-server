6f60e1e751969c80ed4c10e3c6a515d00365680e
raul <develop.raul@gmail.com>
2022-08-22 18:06:58 +0900

    [ADD] DataQueryProperty 작성 및 적용

--- src/main/kotlin/site/iplease/iadserver/IpAssignDemandServerApplication.kt
+++ src/main/kotlin/site/iplease/iadserver/IpAssignDemandServerApplication.kt
@@ -5,11 +5,12 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 import site.iplease.iadserver.domain.accept.config.AcceptedDemandProperties
+import site.iplease.iadserver.domain.query.config.DataQueryProperty
 import site.iplease.iadserver.domain.reject.config.RejectedDemandProperties
 
 @SpringBootApplication
 @EnableScheduling
-@EnableConfigurationProperties(RejectedDemandProperties::class, AcceptedDemandProperties::class)
+@EnableConfigurationProperties(RejectedDemandProperties::class, AcceptedDemandProperties::class, DataQueryProperty::class)
 class IpAssignDemandServerApplication
 
 fun main(args: Array<String>) {
