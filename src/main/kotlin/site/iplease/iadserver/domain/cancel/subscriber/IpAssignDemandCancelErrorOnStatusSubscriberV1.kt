fcbedbd7aa30be7dd191c39515bb012d72d4874d
raul <develop.raul@gmail.com>
2022-08-22 10:57:21 +0900

    [FIX] 알람 내용 수정

--- src/main/kotlin/site/iplease/iadserver/domain/cancel/subscriber/IpAssignDemandCancelErrorOnStatusSubscriberV1.kt
+++ src/main/kotlin/site/iplease/iadserver/domain/cancel/subscriber/IpAssignDemandCancelErrorOnStatusSubscriberV1.kt
@@ -7,17 +7,20 @@ import site.iplease.iadserver.domain.cancel.strategy.DemandCancelCompensateStrat
 import site.iplease.iadserver.domain.cancel.util.DemandCancelErrorOnStatusConverter
 import site.iplease.iadserver.global.cancel.data.message.IpAssignDemandCancelErrorOnStatusMessage
 import site.iplease.iadserver.global.cancel.subscriber.IpAssignDemandCancelErrorOnStatusSubscriber
+import site.iplease.iadserver.infra.alarm.service.PushAlarmService
 
 @Component
 class IpAssignDemandCancelErrorOnStatusSubscriberV1(
     private val demandCancelErrorOnStatusConverter: DemandCancelErrorOnStatusConverter,
-    private val demandCancelCompensateStrategy: DemandCancelCompensateStrategy
+    private val demandCancelCompensateStrategy: DemandCancelCompensateStrategy,
+    private val pushAlarmService: PushAlarmService
 ): IpAssignDemandCancelErrorOnStatusSubscriber {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun subscribe(message: IpAssignDemandCancelErrorOnStatusMessage) {
         demandCancelErrorOnStatusConverter.convert(message)
             .flatMap { demand -> demandCancelCompensateStrategy.compensate(demand) }
+            .flatMap { pushAlarmService.publish(message.issuerId, "IP할당신청을 취소하는중 오류가 발생했어요!", "자세한 내용은 관리자에게 문의해주세요!") }
             .doOnSuccess { logRollback() }
             .doOnError { logError(it) }
             .onErrorResume { Mono.empty() }
--- src/main/kotlin/site/iplease/iadserver/domain/create/subscriber/IpAssignDemandCreateErrorOnStatusSubscriberV1.kt
+++ src/main/kotlin/site/iplease/iadserver/domain/create/subscriber/IpAssignDemandCreateErrorOnStatusSubscriberV1.kt
@@ -7,17 +7,20 @@ import site.iplease.iadserver.domain.create.strategy.DemandCreateCompensateStrat
 import site.iplease.iadserver.domain.create.util.DemandCreateErrorOnStatusConverter
 import site.iplease.iadserver.global.create.data.message.IpAssignDemandCreateErrorOnStatusMessage
 import site.iplease.iadserver.global.create.subscriber.IpAssignDemandCreateErrorOnStatusSubscriber
+import site.iplease.iadserver.infra.alarm.service.PushAlarmService
 
 @Component
 class IpAssignDemandCreateErrorOnStatusSubscriberV1(
     private val demandCreateErrorOnStatusConverter: DemandCreateErrorOnStatusConverter,
-    private val demandCreateCompensateStrategy: DemandCreateCompensateStrategy
+    private val demandCreateCompensateStrategy: DemandCreateCompensateStrategy,
+    private val pushAlarmService: PushAlarmService
 ): IpAssignDemandCreateErrorOnStatusSubscriber {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun subscribe(message: IpAssignDemandCreateErrorOnStatusMessage) {
         demandCreateErrorOnStatusConverter.convert(message)
             .flatMap { demand -> demandCreateCompensateStrategy.compensate(demand) }
+            .flatMap { pushAlarmService.publish(message.issuerId, "IP할당신청을 등록하는중 오류가 발생했어요!", "자세한 내용은 관리자에게 문의해주세요!") }
             .doOnSuccess { logRollback() }
             .doOnError { logError(it) }
             .onErrorResume { Mono.empty() }
